70f15a124410a7de1709fe3ae25684d1159666f0
DESKTOP-BF5T0JB\user <hgji@higenis.com>
2025-02-13 17:16:43 +0900

    09 CAN 예제 추가

--- examples/09_CAN/Core/Src/main.c
+++ examples/09_CAN/Core/Src/main.c
@@ -18,13 +18,14 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "spi.h"
+#include "can.h"
 #include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <stdio.h>
+#include <string.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,16 +35,8 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define W25Q128_CS_LOW()  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET)
-#define W25Q128_CS_HIGH() HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET)
-
-#define W25Q128_CMD_WRITE_ENABLE   0x06
-#define W25Q128_CMD_READ_ID        0x9F
-#define W25Q128_CMD_READ_DATA      0x03
-#define W25Q128_CMD_PAGE_PROGRAM   0x02
-#define W25Q128_CMD_SECTOR_ERASE   0x20
-#define W25Q128_CMD_CHIP_ERASE     0xC7
-#define W25Q128_CMD_READ_STATUS1   0x05
+#define CAN1_ID 0x111   // CAN1 transfer ID
+#define CAN2_ID 0x222   // CAN2 transfer ID
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -54,106 +47,134 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef RxHeader;
+uint8_t TxData[8];
+uint8_t RxData[8];
+uint32_t TxMailbox;
 
+// Test data
+uint8_t can1_tx_data[8] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88};
+uint8_t can2_tx_data[8] = {0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x99, 0x00};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 /* USER CODE BEGIN PFP */
-
+void CAN_Config(void);
+void CAN_Send(CAN_HandleTypeDef *hcan, uint32_t id, uint8_t *data, uint8_t length);
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan);
+int _write(int file, char *ptr, int len);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 int _write(int file, char *ptr, int len)
 {
-  HAL_UART_Transmit(&huart1, (const uint8_t*)ptr, len, 50);
-  return len;
+    HAL_UART_Transmit(&huart1, (uint8_t*)ptr, len, HAL_MAX_DELAY);
+    return len;
 }
 
-///////////////// W25Q128 Functions /////////////////
-// W25Q128 Read ID
-uint32_t W25Q128_ReadID(SPI_HandleTypeDef *hspi) {
-  uint8_t cmd = W25Q128_CMD_READ_ID;
-  uint8_t id[3] = {0};
 
-  W25Q128_CS_LOW();
-  HAL_SPI_Transmit(hspi, &cmd, 1, HAL_MAX_DELAY);
-  HAL_SPI_Receive(hspi, id, 3, HAL_MAX_DELAY);
-  W25Q128_CS_HIGH();
+/* CAN Configuration */
+void CAN_Config()
+{
+    CAN_FilterTypeDef canFilter1;
 
-  printf("\nManufacturer ID: 0x%X, Memory Type: 0x%X, Capacity: 0x%X\n", id[0], id[1], id[2]);
+    // Filter settings (common settings for CAN1 & CAN2)
+    canFilter1.FilterBank = 0;
+    canFilter1.FilterMode = CAN_FILTERMODE_IDMASK;
+    canFilter1.FilterScale = CAN_FILTERSCALE_32BIT;
+    canFilter1.FilterIdHigh = 0x0000;
+    canFilter1.FilterIdLow = 0x0000;
+    canFilter1.FilterMaskIdHigh = 0x0000;
+    canFilter1.FilterMaskIdLow = 0x0000;
+    canFilter1.FilterFIFOAssignment = CAN_RX_FIFO0;
+    canFilter1.FilterActivation = ENABLE;
 
-  return (id[0] << 16) | (id[1] << 8) | id[2];
+    if (HAL_CAN_ConfigFilter(&hcan1, &canFilter1) != HAL_OK)
+    {
+      printf("CAN1 filter setup failed!\n");
+    }
+
+    CAN_FilterTypeDef canFilter2;
+
+    // Filter settings (common settings for CAN1 & CAN2)
+    canFilter2.FilterBank = 1;
+    canFilter2.FilterMode = CAN_FILTERMODE_IDMASK;
+    canFilter2.FilterScale = CAN_FILTERSCALE_32BIT;
+    canFilter2.FilterIdHigh = 0x0000;
+    canFilter2.FilterIdLow = 0x0000;
+    canFilter2.FilterMaskIdHigh = 0x0000;
+    canFilter2.FilterMaskIdLow = 0x0000;
+    canFilter2.FilterFIFOAssignment = CAN_RX_FIFO0;
+    canFilter2.FilterActivation = ENABLE;
+
+    if (HAL_CAN_ConfigFilter(&hcan2, &canFilter2) != HAL_OK)
+    {
+      printf("CAN2 filter setup failed!\n");
+    }
+
+    // Start CAN1 & CAN2
+    HAL_CAN_Start(&hcan1);
+    HAL_CAN_Start(&hcan2);
+
+    // Enable receive interrupt
+    if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+    {
+        printf("CAN1 Interrupt enable failed!\n");
+    }
+
+    if (HAL_CAN_ActivateNotification(&hcan2, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+    {
+        printf("CAN2 Interrupt enable failed!\n");
+    }
 }
 
-// W25Q128 Write Enable
-void W25Q128_WriteEnable(SPI_HandleTypeDef *hspi) {
-  uint8_t cmd = W25Q128_CMD_WRITE_ENABLE;
+/* Sending CAN messages */
+void CAN_Send(CAN_HandleTypeDef *hcan, uint32_t id, uint8_t *data, uint8_t length)
+{
+    TxHeader.StdId = id;
+    TxHeader.ExtId = 0;
+    TxHeader.RTR = CAN_RTR_DATA;
+    TxHeader.IDE = CAN_ID_STD;
+    TxHeader.DLC = length;
+    TxHeader.TransmitGlobalTime = DISABLE;
 
-  W25Q128_CS_LOW();
-  HAL_SPI_Transmit(hspi, &cmd, 1, HAL_MAX_DELAY);
-  W25Q128_CS_HIGH();
+    if (HAL_CAN_AddTxMessage(hcan, &TxHeader, data, &TxMailbox) != HAL_OK)
+    {
+        printf("CAN Send Error!\n");
+    }
 }
 
-/* W25Q128 Write Page
- *
- * Only writeable within page size (256B)!
- * Must erase sectors (4KB) before writing new data.
-*/
-void W25Q128_WritePage(SPI_HandleTypeDef *hspi, uint32_t address, uint8_t *data, uint16_t length) {
-  uint8_t cmd[4];
-
-  if (length > 256) return;  // Page size is limited to 256 bytes
-
-  W25Q128_WriteEnable(hspi);  // Enable writing
-
-  cmd[0] = W25Q128_CMD_PAGE_PROGRAM;
-  cmd[1] = (address >> 16) & 0xFF;
-  cmd[2] = (address >> 8) & 0xFF;
-  cmd[3] = address & 0xFF;
-
-  W25Q128_CS_LOW();
-  HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
-  HAL_SPI_Transmit(hspi, data, length, HAL_MAX_DELAY);
-  W25Q128_CS_HIGH();
-
-  HAL_Delay(50);
-}
-
-// W25Q128 Erase sector
-void W25Q128_EraseSector(SPI_HandleTypeDef *hspi, uint32_t address) {
-  uint8_t cmd[4];
-
-  W25Q128_WriteEnable(hspi);  // Enable writing
-
-  cmd[0] = W25Q128_CMD_SECTOR_ERASE;
-  cmd[1] = (address >> 16) & 0xFF;
-  cmd[2] = (address >> 8) & 0xFF;
-  cmd[3] = address & 0xFF;
-
-  W25Q128_CS_LOW();
-  HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
-  W25Q128_CS_HIGH();
-
-  HAL_Delay(50);  // Wait for sector erase (up to 50ms)
-}
-
-// Read Data from W25Q128
-void W25Q128_ReadData(SPI_HandleTypeDef *hspi, uint32_t address, uint8_t *buffer, uint16_t length) {
-  uint8_t cmd[4];
-
-  cmd[0] = W25Q128_CMD_READ_DATA;
-  cmd[1] = (address >> 16) & 0xFF;
-  cmd[2] = (address >> 8) & 0xFF;
-  cmd[3] = address & 0xFF;
-
-  W25Q128_CS_LOW();
-  HAL_SPI_Transmit(hspi, cmd, 4, HAL_MAX_DELAY);
-  HAL_SPI_Receive(hspi, buffer, length, HAL_MAX_DELAY);
-  W25Q128_CS_HIGH();
-
-  HAL_Delay(50);
+/* CAN receive interrupt callback function */
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+    if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
+    {
+        if (hcan == &hcan1)
+        {
+            printf("CAN1 Receive: ID=0x%lX, Data: ", RxHeader.StdId);
+            for (int i = 0; i < RxHeader.DLC; i++)
+            {
+                printf("%02X ", RxData[i]);
+            }
+            printf("\n[O] CAN1: Receive normal data!\n\n");
+        }
+        else if (hcan == &hcan2)
+        {
+            printf("CAN2 Receive: ID=0x%lX, Data: ", RxHeader.StdId);
+            for (int i = 0; i < RxHeader.DLC; i++)
+            {
+                printf("%02X ", RxData[i]);
+            }
+            printf("\n[O] CAN2: Receive normal data!\n\n");
+        }
+    }
+    else
+    {
+        printf("[X] CAN RX Failed to read message!\n");
+    }
 }
 
 /* USER CODE END 0 */
@@ -187,34 +208,14 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_SPI2_Init();
+  MX_CAN1_Init();
+  MX_CAN2_Init();
   MX_USART1_UART_Init();
 
   /* USER CODE BEGIN 2 */
-  uint8_t writeBuffer[] = "Hello, W25Q128!";
-  uint8_t readBuffer[20] = {0,};
-  uint32_t flash_address = 0x000100;
+  printf("STM32 CAN1 <-> CAN2 Test\n");
 
-  printf("\nW25Q128 Read ID");
-  W25Q128_ReadID(&hspi2);
-
-  printf("\nW25Q128 Erase Sector");
-  W25Q128_EraseSector(&hspi2, flash_address);
-
-  printf("\nW25Q128 Write Data\n");
-  W25Q128_WritePage(&hspi2, flash_address, writeBuffer, sizeof(writeBuffer));
-
-  printf("\nW25Q128 Read Data\r\n");
-  //  memset(readBuffer, 0, sizeof(readBuffer));
-  W25Q128_ReadData(&hspi2, flash_address, readBuffer, sizeof(readBuffer));
-
-  printf("readData: %s\n", readBuffer);
-
-  // Modified to output only valid data
-  char *validData = (char *)readBuffer;
-  while (*validData == 0xFF) validData++;  // Skip 0xFF values
-
-  printf("valid readData: %s\n", validData);
+  CAN_Config();
 
   /* USER CODE END 2 */
 
@@ -225,6 +226,13 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+    printf("Send CAN1 -> CAN2\n");
+    CAN_Send(&hcan1, CAN1_ID, can1_tx_data, 8);
+    HAL_Delay(2000);
+
+    printf("Send CAN2 -> CAN1\n");
+    CAN_Send(&hcan2, CAN2_ID, can2_tx_data, 8);
+    HAL_Delay(2000);
   }
   /* USER CODE END 3 */
 }
--- examples/09_CAN/Core/Src/usart.c
+++ examples/09_CAN/Core/Src/usart.c
@@ -77,7 +77,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-     GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+    GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN USART1_MspInit 1 */
